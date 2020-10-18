04d8251f5a6136e85f0ae277ba0f7057d2327190
FreddyPatarroyo <frpatarroyo@poligran.edu.co>
2020-10-17 21:05:41 -0500

    primera subida del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ItemComponent } from './galeria/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GaleriaComponent,
+    FormularioComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
