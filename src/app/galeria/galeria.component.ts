6f7e800ef06a908c5c90bb8f6228942704029a4c
Danny8ape <daochoap@poligran.edu.co>
2020-10-19 15:48:35 -0500

    Limite

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -16,6 +16,7 @@ export class GaleriaComponent implements OnInit {
   size : string;
   mensaje: boolean;
   
+  
   constructor(private itemServices: ItemServices) {
     this.size="";
     this.pagFiltro ="0";
